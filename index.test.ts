363874b9f9eba0fc0f223fb48dce943dca22b682
A-yon Lee <the@ayon.li>
2023-09-14 10:56:18 +0800

    add jsext.read()

--- index.test.ts
+++ index.test.ts
@@ -1,7 +1,13 @@
 import jsext from "./index";
 import { words } from "./string";
 import { describe, test } from "mocha";
-import { deepStrictEqual, strictEqual } from "assert";
+import { deepStrictEqual, ok, strictEqual } from "assert";
+import { createReadStream } from "fs";
+import * as http from "http";
+import SSE from "@ayonli/sse";
+import dEventSource = require("eventsource");
+import { server as WebSocketServer, w3cwebsocket } from "websocket";
+import EventEmitter = require("events");
 
 describe("jsext", () => {
     test("jsext.wrap", () => {
@@ -414,6 +420,190 @@ describe("jsext", () => {
             }
 
             deepStrictEqual(logs2, ["Hello", "2", "1", undefined]);
+        });
+    });
+
+    describe("jsext.read", () => {
+        test("ReadableStream", async () => {
+            const file = createReadStream("./package.json");
+            const chunks: Buffer[] = [];
+
+            for await (const chunk of jsext.read(file)) {
+                chunks.push(chunk as Buffer);
+            }
+
+            ok(chunks.length > 0);
+        });
+
+        test("EventSource", jsext.func(async (defer) => {
+            const server = http.createServer((req, res) => {
+                if (!SSE.isEventSource(req)) {
+                    res.end();
+                    return;
+                }
+
+                const sse = new SSE(req, res);
+
+                if (req.url === "/message") {
+                    sse.send("hello");
+                    sse.send("world");
+                } else if (req.url === "/event") {
+                    sse.emit("reply", "foo");
+                    sse.emit("reply", "bar");
+                } else {
+                    res.end();
+                }
+            }).listen(12345);
+            defer(() => new Promise(resolve => {
+                server.closeAllConnections();
+                server.close(resolve);
+            }));
+
+            const es1 = new dEventSource("http://localhost:12345/message") as EventSource;
+            const messages: string[] = [];
+
+            for await (const msg of jsext.read(es1)) {
+                messages.push(msg);
+
+                if (messages.length === 2) {
+                    es1.close();
+                }
+            }
+
+            deepStrictEqual(messages, ["hello", "world"]);
+
+            const es2 = new dEventSource("http://localhost:12345/event") as EventSource;
+            const replies: string[] = [];
+
+            for await (const reply of jsext.read(es2, { event: "reply" })) {
+                replies.push(reply);
+
+                if (replies.length === 2) {
+                    es2.close();
+                }
+            }
+
+            deepStrictEqual(replies, ["foo", "bar"]);
+        }));
+
+        test("WebSocket", jsext.func(async (defer) => {
+            let server = http.createServer().listen(12345);
+            let wsServer = new WebSocketServer({
+                httpServer: server
+            });
+            defer(() => new Promise(resolve => {
+                server.closeAllConnections();
+                server.close(resolve);
+            }));
+
+            wsServer.on("request", async req => {
+                let conn = req.accept('echo-protocol', req.origin);
+
+                for (let msg of ["hello", "world"]) {
+                    conn.send(msg);
+                }
+            });
+
+            const ws = new w3cwebsocket(
+                "ws://localhost:12345",
+                "echo-protocol") as unknown as WebSocket;
+            const messages: string[] = [];
+
+            for await (const msg of jsext.read<string>(ws)) {
+                messages.push(msg);
+
+                if (messages.length === 2) {
+                    ws.close();
+                }
+            }
+
+            deepStrictEqual(messages, ["hello", "world"]);
+        }));
+
+        test("EventTarget", async () => {
+            const target1 = new EventTarget();
+            const messages: string[] = [];
+
+            (async () => {
+                await Promise.resolve(null);
+                target1.dispatchEvent(new MessageEvent("message", { data: "hello" }));
+                target1.dispatchEvent(new MessageEvent("message", { data: "world" }));
+            })();
+
+            for await (const msg of jsext.read<string>(target1)) {
+                messages.push(msg);
+
+                if (messages.length === 2) {
+                    target1.dispatchEvent(new Event("close"));
+                }
+            }
+
+            deepStrictEqual(messages, ["hello", "world"]);
+
+            const target2 = new EventTarget();
+            const messages2: string[] = [];
+
+            (async () => {
+                await Promise.resolve(null);
+                target2.dispatchEvent(new MessageEvent("reply", { data: "foo" }));
+                target2.dispatchEvent(new MessageEvent("reply", { data: "bar" }));
+            })();
+
+            for await (const msg of jsext.read<string>(target2, {
+                message: "reply",
+                close: "end",
+            })) {
+                messages2.push(msg);
+
+                if (messages2.length === 2) {
+                    target2.dispatchEvent(new Event("end"));
+                }
+            }
+
+            deepStrictEqual(messages2, ["foo", "bar"]);
+        });
+
+        test("EventEmitter", async () => {
+            const target1 = new EventEmitter();
+            const messages: string[] = [];
+
+            (async () => {
+                await Promise.resolve(null);
+                target1.emit("data", "hello");
+                target1.emit("data", "world");
+            })();
+
+            for await (const msg of jsext.read<string>(target1)) {
+                messages.push(msg);
+
+                if (messages.length === 2) {
+                    target1.emit("close");
+                }
+            }
+
+            deepStrictEqual(messages, ["hello", "world"]);
+
+            const target2 = new EventEmitter();
+            const messages2: string[] = [];
+
+            (async () => {
+                await Promise.resolve(null);
+                target2.emit("reply","foo");
+                target2.emit("reply", "bar");
+            })();
+
+            for await (const msg of jsext.read<string>(target2, {
+                data: "reply",
+                close: "end",
+            })) {
+                messages2.push(msg);
+
+                if (messages2.length === 2) {
+                    target2.emit("end");
+                }
+            }
+
+            deepStrictEqual(messages2, ["foo", "bar"]);
         });
     });
 });
