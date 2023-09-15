9136f51b7552ac881146c9876484b699bd0af15a
A-yon Lee <the@ayon.li>
2023-09-16 00:12:02 +0800

    add jsext.throttle

--- index.test.ts
+++ index.test.ts
@@ -1,6 +1,7 @@
 import jsext from "./index";
 import { words } from "./string";
 import { hasOwn } from "./object";
+import { sleep } from "./promise";
 import { describe, test } from "mocha";
 import { deepStrictEqual, ok, strictEqual } from "assert";
 import { createReadStream } from "fs";
@@ -422,6 +423,222 @@ describe("jsext", () => {
         strictEqual(wrapped.name, echo.name);
         strictEqual(wrapped.length, echo.length);
         strictEqual(wrapped.toString(), echo.toString());
+    });
+
+    describe("jsext.throttle", () => {
+        describe("regular function", () => {
+            describe("without key", () => {
+                test("success", async () => {
+                    const fn = jsext.throttle(<T>(obj: T) => obj, 5);
+                    const res1 = fn({ foo: "hello", bar: "world" });
+                    const res2 = fn({ foo: "hello" });
+
+                    deepStrictEqual(res1, { foo: "hello", bar: "world" });
+                    ok(res1 === res2);
+                    await sleep(6);
+
+                    const res3 = fn({ bar: "world" });
+                    deepStrictEqual(res3, { bar: "world" });
+                    ok(res1 !== res3);
+                });
+
+                test("failure", async () => {
+                    const fn = jsext.throttle(<T>(obj: T) => {
+                        if (true) {
+                            throw new Error("something went wrong");
+                        } else {
+                            return obj;
+                        }
+                    }, 5);
+                    const [err1] = jsext.try(() => fn({ foo: "hello", bar: "world" }));
+                    const [err2] = jsext.try(() => fn({ foo: "hello" }));
+
+                    deepStrictEqual(err1, new Error("something went wrong"));
+                    ok(err1 === err2);
+
+                    await sleep(6);
+
+                    const [err3] = jsext.try(() => fn({ bar: "world" }));
+                    deepStrictEqual(err3, new Error("something went wrong"));
+                    ok(err1 !== err3);
+                });
+            });
+
+            describe("with key", () => {
+                test("success", async () => {
+                    const res1 = jsext.throttle(<T>(obj: T) => obj, {
+                        duration: 5,
+                        for: "foo",
+                    })({ foo: "hello", bar: "world" });
+                    const res2 = jsext.throttle(<T>(obj: T) => obj, {
+                        duration: 5,
+                        for: "foo",
+                    })({ foo: "hello" });
+
+                    deepStrictEqual(res1, { foo: "hello", bar: "world" });
+                    ok(res1 === res2);
+                    await sleep(6);
+
+                    const res3 = jsext.throttle(<T>(obj: T) => obj, {
+                        duration: 5,
+                        for: "foo",
+                    })({ bar: "world" });
+                    deepStrictEqual(res3, { bar: "world" });
+                    ok(res1 !== res3);
+                });
+
+                test("failure", async () => {
+                    const [err1] = jsext.try(() => jsext.throttle(<T>(obj: T) => {
+                        if (true) {
+                            throw new Error("something went wrong");
+                        } else {
+                            return obj;
+                        }
+                    }, {
+                        duration: 5,
+                        for: "bar",
+                    })({ foo: "hello", bar: "world" }));
+                    const [err2] = jsext.try(() => jsext.throttle(<T>(obj: T) => {
+                        if (true) {
+                            throw new Error("something went wrong");
+                        } else {
+                            return obj;
+                        }
+                    }, {
+                        duration: 5,
+                        for: "bar",
+                    })({ foo: "hello" }));
+
+                    deepStrictEqual(err1, new Error("something went wrong"));
+                    ok(err1 === err2);
+
+                    await sleep(6);
+
+                    const [err3] = jsext.try(() => jsext.throttle(<T>(obj: T) => {
+                        if (true) {
+                            throw new Error("something went wrong");
+                        } else {
+                            return obj;
+                        }
+                    }, {
+                        duration: 5,
+                        for: "bar",
+                    })({ bar: "world" }));
+                    deepStrictEqual(err3, new Error("something went wrong"));
+                    ok(err1 !== err3);
+                });
+            });
+        });
+
+        describe("async function", () => {
+            describe("without key", () => {
+                test("success", async () => {
+                    const fn = jsext.throttle(<T>(obj: T) => Promise.resolve(obj), 5);
+                    const [res1, res2] = await Promise.all([
+                        fn({ foo: "hello", bar: "world" }),
+                        fn({ foo: "hello" }),
+                    ]);
+
+                    ok(res1 === res2);
+                    await sleep(6);
+
+                    const res3 = await fn({ bar: "world" });
+                    deepStrictEqual(res3, { bar: "world" });
+                    ok(res1 !== res3);
+                });
+
+                test("failure", async () => {
+                    const fn = jsext.throttle(<T>(obj: T) => {
+                        if (true) {
+                            return Promise.reject(new Error("something went wrong"));
+                        } else {
+                            return Promise.resolve(obj);
+                        }
+                    }, 5);
+                    const [[err1], [err2]] = await Promise.all([
+                        jsext.try(fn({ foo: "hello", bar: "world" })),
+                        jsext.try(fn({ foo: "hello" })),
+                    ]);
+
+                    deepStrictEqual(err1, new Error("something went wrong"));
+                    ok(err1 === err2);
+
+                    await sleep(6);
+
+                    const [err3] = await jsext.try(fn({ bar: "world" }));
+                    deepStrictEqual(err3, new Error("something went wrong"));
+                    ok(err1 !== err3);
+                });
+            });
+
+            describe("with key", () => {
+                test("success", async () => {
+                    const [res1, res2] = await Promise.all([
+                        jsext.throttle(<T>(obj: T) => Promise.resolve(obj), {
+                            duration: 5,
+                            for: "foo",
+                        })({ foo: "hello", bar: "world" }),
+                        jsext.throttle(<T>(obj: T) => Promise.resolve(obj), {
+                            duration: 5,
+                            for: "foo",
+                        })({ foo: "hello" }),
+                    ]);
+
+                    ok(res1 === res2);
+                    await sleep(6);
+
+                    const res3 = await jsext.throttle(<T>(obj: T) => Promise.resolve(obj), {
+                        duration: 5,
+                        for: "foo",
+                    })({ bar: "world" });
+                    deepStrictEqual(res3, { bar: "world" });
+                    ok(res1 !== res3);
+                });
+
+                test("failure", async () => {
+                    const [[err1], [err2]] = await Promise.all([
+                        jsext.try(jsext.throttle(<T>(obj: T) => {
+                            if (true) {
+                                return Promise.reject(new Error("something went wrong"));
+                            } else {
+                                return Promise.resolve(obj);
+                            }
+                        }, {
+                            duration: 5,
+                            for: "bar",
+                        })({ foo: "hello", bar: "world" })),
+                        jsext.try(jsext.throttle(<T>(obj: T) => {
+                            if (true) {
+                                return Promise.reject(new Error("something went wrong"));
+                            } else {
+                                return Promise.resolve(obj);
+                            }
+                        }, {
+                            duration: 5,
+                            for: "bar",
+                        })({ foo: "hello" })),
+                    ]);
+
+                    deepStrictEqual(err1, new Error("something went wrong"));
+                    ok(err1 === err2);
+
+                    await sleep(6);
+
+                    const [err3] = await jsext.try(jsext.throttle(<T>(obj: T) => {
+                        if (true) {
+                            return Promise.reject(new Error("something went wrong"));
+                        } else {
+                            return Promise.resolve(obj);
+                        }
+                    }, {
+                        duration: 5,
+                        for: "bar",
+                    })({ bar: "world" }));
+                    deepStrictEqual(err3, new Error("something went wrong"));
+                    ok(err1 !== err3);
+                });
+            });
+        });
     });
 
     test("jsext.mixins", () => {
--- index.ts
+++ index.ts
@@ -12,6 +12,13 @@ export type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) ex
 
 const isNode = typeof process === "object" && process.versions?.node;
 
+type ThrottleCache = {
+    for: any;
+    expires?: number;
+    result?: { value?: any; error?: unknown; };
+};
+const throttleCaches = new Map<any, ThrottleCache>();
+
 /**
  * The maximum number of workers is set to 4 times of the CPU core numbers.
  * 
@@ -156,6 +163,25 @@ export interface JsExt {
         fn: Fn,
         wrapper: (this: T, fn: Fn, ...args: Parameters<Fn>) => ReturnType<Fn>
     ): Fn;
+
+    /**
+     * Creates a throttled function that will only be run once in a certain amount of time.
+     * 
+     * If a subsequent call happens within the `duration`, the previous result will be returned and
+     * the `handler` function will not be invoked.
+     */
+    throttle<T, Fn extends (this: T, ...args: any[]) => any>(handler: Fn, duration: number): Fn;
+    throttle<T, Fn extends (this: T, ...args: any[]) => any>(handler: Fn, options: {
+        duration: number;
+        /**
+         * Use the throttle strategy `for` the given key, this will keep the result in a global
+         * cache, binding new `handler` function for the same key will result in the same result
+         * as the previous, unless the duration has passed. This mechanism guarantees that both
+         * creating the throttled function in function scopes and overwriting the handler are
+         * possible.
+         */
+        for?: any;
+    }): Fn;
 
     /**
      * Returns an extended class that combines all mixin methods.
@@ -432,6 +458,53 @@ const jsext: JsExt = {
         });
 
         return wrapped as Fn;
+    },
+    throttle(handler, options) {
+        const key = typeof options === "number" ? null : options.for;
+        const duration = typeof options === "number" ? options : options.duration;
+
+        const handleCall = function (
+            this: any,
+            cache: ThrottleCache,
+            ...args: any[]
+        ) {
+            if (cache.result && Date.now() < (cache.expires ?? 0)) {
+                if (cache.result.error) {
+                    throw cache.result.error;
+                } else {
+                    return cache.result.value;
+                }
+            }
+
+            try {
+                const returns = handler.call(this, ...args);
+                cache.result = { value: returns };
+                cache.expires = Date.now() + duration;
+                return returns;
+            } catch (error) {
+                cache.result = { error };
+                cache.expires = Date.now() + duration;
+                throw error;
+            }
+        };
+
+        if (!key) {
+            const cache: ThrottleCache = { for: null };
+            return function (this: any, ...args: any[]) {
+                return handleCall.call(this, cache, ...args);
+            };
+        } else {
+            let cache = throttleCaches.get(key);
+
+            if (!cache) {
+                cache = { for: key };
+                throttleCaches.set(key, cache);
+            }
+
+            return function (this: any, ...args: any[]) {
+                return handleCall.call(this, cache as ThrottleCache, ...args);
+            };
+        }
     },
     mixins(base, ...mixins) {
         const obj = { ctor: null as any as Constructor<any> };
