ac9f435f3ed0fb921e66d29040c891b1a6444eee
A-yon Lee <the@ayon.li>
2023-09-20 03:54:27 +0800

    remove unintended type declaration

--- test-deno.ts
+++ test-deno.ts
@@ -2,7 +2,6 @@ import "https://unpkg.com/mocha@10.2.0/mocha.js";
 import { dirname, fromFileUrl } from "https://deno.land/std@0.201.0/path/mod.ts";
 import { globber } from "https://deno.land/x/globber@0.1.0/mod.ts";
 
-declare var Deno: any;
 (window as any).location = new URL("http://localhost:0");
 mocha.setup({ ui: "bdd", reporter: "spec" });
 mocha.checkLeaks();
