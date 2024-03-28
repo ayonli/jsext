c54813ac7b64cc150113d1d278fb0c5b5b1595bc
A-yon Lee <the@ayon.li>
2024-03-28 14:13:26 +0800

    rebuild

--- esm/dialog/util.js
+++ esm/dialog/util.js
@@ -12,10 +12,12 @@ async function questionInRepl(message, defaultValue = "") {
     const { stdin, stdout } = process;
     return await hijackStdin(stdin, async () => {
         const buf = [];
+        let cursor = 0;
         stdout.write(message);
         if (defaultValue) {
             stdout.write(defaultValue);
             buf.push(...defaultValue);
+            cursor += defaultValue.length;
         }
         const answer = await new Promise(resolve => {
             const listener = (char, key) => {
@@ -28,15 +30,41 @@ async function questionInRepl(message, defaultValue = "") {
                     resolve(buf.join(""));
                 }
                 else if (key.name === "backspace") {
-                    if (buf.length) {
+                    if (cursor > 0) {
+                        buf.splice(cursor - 1, 1);
+                        cursor--;
+                        const rest = buf.slice(cursor).join("");
                         stdout.moveCursor(-1, 0);
                         stdout.clearLine(1);
-                        buf.pop();
+                        stdout.write(rest);
+                        stdout.moveCursor(-rest.length, 0);
                     }
                 }
                 else if (char !== undefined) {
-                    stdout.write(char);
-                    buf.push(char);
+                    if (cursor === buf.length) {
+                        stdout.write(char);
+                        buf.push(char);
+                        cursor++;
+                    }
+                    else {
+                        buf.splice(cursor, 0, char);
+                        const rest = buf.slice(cursor + 1).join("");
+                        stdout.write(char + rest);
+                        cursor++;
+                        stdout.moveCursor(-rest.length, 0);
+                    }
+                }
+                else if (key.name === "left") {
+                    if (cursor > 0) {
+                        stdout.moveCursor(-1, 0);
+                        cursor--;
+                    }
+                }
+                else if (key.name === "right") {
+                    if (cursor < buf.length) {
+                        stdout.moveCursor(1, 0);
+                        cursor++;
+                    }
                 }
             };
             stdin.on("keypress", listener);
