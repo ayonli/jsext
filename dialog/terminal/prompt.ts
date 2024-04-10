edb5674505f9968fcb16ddd018d283b02a9eae0d
A-yon Lee <the@ayon.li>
2024-04-10 13:45:45 +0800

    optimize the GUI prompt dialog for Windows

--- dialog/terminal/prompt.ts
+++ dialog/terminal/prompt.ts
@@ -11,9 +11,55 @@ function createAppleScript(message: string, defaultValue = "", password = false)
 }
 
 function createPowerShellScript(message: string, defaultValue = "", password = false) {
-    void password; // password is not supported in PowerShell
-    return "Add-Type -AssemblyName Microsoft.VisualBasic;"
-        + `[Microsoft.VisualBasic.Interaction]::InputBox("${escape(message)}", "Prompt", "${escape(defaultValue)}");`;
+    return [
+        "$form = New-Object System.Windows.Forms.Form",
+        "$form.Text = 'Prompt'",
+        "$form.Size = New-Object System.Drawing.Size(450,200)",
+        "$form.StartPosition = 'CenterScreen'",
+        "",
+        "$label = New-Object System.Windows.Forms.Label",
+        "$label.Location = New-Object System.Drawing.Point(10,20)",
+        "$label.Size = New-Object System.Drawing.Size(400,20)",
+        `$label.Text = "${escape(message)}"`,
+        "$form.Controls.Add($label)",
+        "",
+        "$textBox = New-Object System.Windows.Forms.TextBox",
+        "$textBox.Location = New-Object System.Drawing.Point(10,40)",
+        "$textBox.Size = New-Object System.Drawing.Size(400,20)",
+        `$textBox.UseSystemPasswordChar = ${password ? "$true" : "$false"}`,
+        `$textBox.Text = "${escape(defaultValue)}"`,
+        "$form.Controls.Add($textBox)",
+        "",
+        "$cancelButton = New-Object System.Windows.Forms.Button",
+        "$cancelButton.Location = New-Object System.Drawing.Point(100,70)",
+        "$cancelButton.Size = New-Object System.Drawing.Size(75,23)",
+        "$cancelButton.Text = 'Cancel'",
+        "$cancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel",
+        "$form.Controls.Add($cancelButton)",
+        "",
+        "$okButton = New-Object System.Windows.Forms.Button",
+        "$okButton.Location = New-Object System.Drawing.Point(175,70)",
+        "$okButton.Size = New-Object System.Drawing.Size(75,23)",
+        "$okButton.Text = 'OK'",
+        "$okButton.DialogResult = [System.Windows.Forms.DialogResult]::OK",
+        "$form.Controls.Add($okButton)",
+        "",
+        "$form.AcceptButton = $okButton",
+        "",
+        "$form.Topmost = $true",
+        "$form.Add_Shown({$textBox.Select()})",
+        "",
+        "$result = $form.ShowDialog()",
+        "",
+        "if ($result -eq [System.Windows.Forms.DialogResult]::OK)",
+        "{",
+        "    $textBox.Text",
+        "}",
+        "elseif ($result -eq [System.Windows.Forms.DialogResult]::Cancel)",
+        "{",
+        "    throw 'User canceled'",
+        "}",
+    ].join("\n");
 }
 
 export default async function promptInTerminal(message: string, options: {
@@ -72,7 +118,11 @@ export default async function promptInTerminal(message: string, options: {
         ]);
 
         if (code) {
-            throw new Error(stderr);
+            if (stderr.includes("User canceled")) {
+                return null;
+            } else {
+                throw new Error(stderr);
+            }
         } else {
             return stdout.trim();
         }
