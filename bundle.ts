2e84c6d7bd1cb8fdbd47b4c222459735182db563
A-yon Lee <i@hyurl.com>
2024-04-18 23:59:31 +0800

    fix bundle exports

--- bundle.ts
+++ bundle.ts
@@ -1,33 +1,41 @@
 import "./augment.ts";
 import jsext from "./index.ts";
-import * as string from "./string.ts";
-import * as number from "./number.ts";
 import * as array from "./array.ts";
-import * as object from "./object.ts";
+import * as _async from "./async.ts";
+import * as bytes from "./bytes.ts";
+import * as _class from "./class.ts";
+import * as cli from "./cli.ts";
+import * as collections from "./collections.ts";
+import * as dialog from "./dialog.ts";
+import * as error from "./error.ts";
+import * as filetype from "./filetype.ts";
 import * as json from "./json.ts";
 import * as math from "./math.ts";
-import * as promise from "./async.ts";
-import * as error from "./error.ts";
-import * as collections from "./collections.ts";
-import * as bytes from "./bytes.ts";
+import * as module from "./module.ts";
+import * as number from "./number.ts";
+import * as object from "./object.ts";
 import * as path from "./path.ts";
-import * as dialog from "./dialog.ts";
+import * as string from "./string.ts";
 
 export default {
     ...jsext,
-    string,
-    number,
     array,
-    object,
-    json,
-    math,
-    promise,
-    error,
-    collections,
+    async: _async,
     bytes: {
         ...object.omit(bytes, ["default"]),
         bytes: bytes.default,
     },
-    path,
+    class: _class,
+    cli,
+    collections,
     dialog,
+    error,
+    filetype,
+    json,
+    math,
+    module,
+    number,
+    object,
+    path,
+    string,
 };
