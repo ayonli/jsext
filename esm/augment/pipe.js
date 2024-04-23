5dd6c9fe6a8ec8af6a1a1c8d7262258f1cb878cc
A-yon Lee <the@ayon.li>
2024-04-23 10:07:17 +0800

    rebuild

--- esm/augment/pipe.js
+++ esm/augment/pipe.js
@@ -1,15 +1,33 @@
 import { Pipeline } from '../pipe.js';
 
 function pipe(fn, ...args) {
-    return new Pipeline(this instanceof Date ? this : this.valueOf()).pipe(fn, ...args);
+    if ([String, Number, BigInt, Boolean, Symbol].includes(this.constructor)) {
+        return new Pipeline(this.valueOf()).pipe(fn, ...args);
+    }
+    else {
+        return new Pipeline(this).pipe(fn, ...args);
+    }
 }
 String.prototype.pipe = pipe;
 Number.prototype.pipe = pipe;
 BigInt.prototype.pipe = pipe;
 Boolean.prototype.pipe = pipe;
-Array.prototype.pipe = pipe;
+Symbol.prototype.pipe = pipe;
+Date.prototype.pipe = pipe;
+RegExp.prototype.pipe = pipe;
+Error.prototype.pipe = pipe;
 Map.prototype.pipe = pipe;
 Set.prototype.pipe = pipe;
-Error.prototype.pipe = pipe;
-Date.prototype.pipe = pipe;
+Array.prototype.pipe = pipe;
+Int8Array.prototype.pipe = pipe;
+Uint8Array.prototype.pipe = pipe;
+Uint8ClampedArray.prototype.pipe = pipe;
+Int16Array.prototype.pipe = pipe;
+Uint16Array.prototype.pipe = pipe;
+Int32Array.prototype.pipe = pipe;
+Uint32Array.prototype.pipe = pipe;
+Float32Array.prototype.pipe = pipe;
+Float64Array.prototype.pipe = pipe;
+BigInt64Array.prototype.pipe = pipe;
+BigUint64Array.prototype.pipe = pipe;
 //# sourceMappingURL=pipe.js.map
