a1f1925cf3f96f33e314195043f396b122e6cb06
A-yon Lee <the@ayon.li>
2024-05-22 10:46:19 +0800

    improve tests for untar()

--- archive/untar.test.ts
+++ archive/untar.test.ts
@@ -1,8 +1,8 @@
-import { deepStrictEqual } from "node:assert";
+import { deepStrictEqual, strictEqual } from "node:assert";
 import tar from "./tar.ts";
 import { pick } from "../object.ts";
-import { TarEntry } from "./Tarball.ts";
-import { DirEntry, readDir, remove } from "../fs.ts";
+import Tarball, { TarEntry } from "./Tarball.ts";
+import { DirEntry, createWritableStream, mkdir, readDir, remove, stat } from "../fs.ts";
 import func from "../func.ts";
 import untar from "./untar.ts";
 import { readAsArray } from "../reader.ts";
@@ -118,5 +118,39 @@ describe("archive/untar", () => {
                 relativePath: join("fs", "util.ts"),
             },
         ] as Partial<DirEntry>[]);
+    }));
+
+    it("extract tarball files and restore metadata", func(async (defer) => {
+        const dir = "./tmp";
+        await mkdir(dir);
+        defer(() => remove(dir, { recursive: true }));
+
+        const tarball = new Tarball();
+
+        tarball.append(null, {
+            kind: "directory",
+            relativePath: "foo",
+            mode: 0o740,
+            mtime: new Date(0),
+        });
+        tarball.append("Hello, World!", {
+            relativePath: "foo/hello.txt",
+            mode: 0o740,
+            mtime: new Date(0),
+        });
+
+        const filename = "./tmp/foo.tar";
+        const output = createWritableStream(filename);
+
+        await tarball.stream().pipeTo(output);
+        await untar(filename, dir);
+
+        const stat1 = await stat("./tmp/foo");
+        strictEqual(stat1.mode & 0o777, 0o740);
+        strictEqual(stat1.mtime?.getTime(), 0);
+
+        const stat2 = await stat("./tmp/foo/hello.txt");
+        strictEqual(stat2.mode & 0o777, 0o740);
+        strictEqual(stat2.mtime?.getTime(), 0);
     }));
 });
