0547cd8fb3a1ff74181859e35460260d061bc22c
A-yon Lee <the@ayon.li>
2024-05-29 11:21:26 +0800

    fix exports of the dialog module

--- dialog.ts
+++ dialog.ts
@@ -8,14 +8,32 @@
 
 import progress from "./dialog/progress.ts";
 import type { ProgressState, ProgressFunc, ProgressAbortHandler } from "./dialog/progress.ts";
-import { openFile, pickFile, pickFiles, pickDirectory, saveFile, downloadFile } from "./dialog/file.ts";
+import {
+    openFile,
+    openFiles,
+    openDirectory,
+    pickFile,
+    pickFiles,
+    pickDirectory,
+    saveFile,
+    downloadFile,
+} from "./dialog/file.ts";
 import { alertInBrowser, confirmInBrowser, promptInBrowser } from "./dialog/browser/index.ts";
 import alertInTerminal from "./dialog/terminal/alert.ts";
 import confirmInTerminal from "./dialog/terminal/confirm.ts";
 import promptInTerminal from "./dialog/terminal/prompt.ts";
 import { isBrowserWindow, isDeno, isNodeLike } from "./env.ts";
 
-export { openFile, pickFile, pickFiles, pickDirectory, saveFile, downloadFile };
+export {
+    openFile,
+    openFiles,
+    openDirectory,
+    pickFile,
+    pickFiles,
+    pickDirectory,
+    saveFile,
+    downloadFile,
+};
 export { progress, ProgressState, ProgressFunc, ProgressAbortHandler };
 
 /**
