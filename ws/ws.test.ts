0adebc43bc7e7dfb3471f78120e881af3239bf39
A-yon Lee <i@hyurl.com>
2024-06-10 14:01:02 +0800

    add test for ws module with express framework

--- ws/ws.test.ts
+++ ws/ws.test.ts
@@ -432,4 +432,87 @@ describe("ws:node", () => {
         strictEqual(clientMessages[0], "client sent: text");
         strictEqual(text(clientMessages[1] as Uint8Array), "client sent: binary");
     }));
+
+    it("with Express framework", func(async function (defer) {
+        const { WebSocketServer } = await import("../ws/node.ts");
+        const { default: express } = await import("express");
+
+        let errorEvent: ErrorEvent | undefined;
+        let closeEvent: CloseEvent | undefined;
+        const serverMessages: (string | Uint8Array)[] = [];
+        const clientMessages: (string | Uint8Array)[] = [];
+
+        const wsServer = new WebSocketServer((socket) => {
+            socket.addEventListener("message", (event) => {
+                if (typeof event.data === "string") {
+                    serverMessages.push(event.data);
+                    socket.send("client sent: " + event.data);
+                } else {
+                    serverMessages.push(event.data);
+                    socket.send(concat(bytes("client sent: "), event.data));
+                }
+            });
+
+            socket.addEventListener("error", (ev) => {
+                errorEvent = ev;
+            });
+
+            socket.addEventListener("close", (ev) => {
+                closeEvent = ev;
+            });
+        });
+
+        const app = express();
+
+        app.get("/", (req) => {
+            wsServer.upgrade(req);
+        });
+
+        const port = await randomPort();
+        const server = app.listen(port);
+        defer(() => server.close());
+
+        let ws: WebSocket;
+
+        if (typeof WebSocket === "function") {
+            ws = new WebSocket(`ws://localhost:${port}`);
+        } else {
+            const dWebSocket = (await import("isomorphic-ws")).default;
+            ws = new dWebSocket(`ws://localhost:${port}`) as unknown as WebSocket;
+        }
+
+        ws.binaryType = "arraybuffer";
+        ws.onopen = () => {
+            ws.send("text");
+        };
+
+        ws.onmessage = (event) => {
+            if (typeof event.data === "string") {
+                clientMessages.push(event.data);
+            } else {
+                clientMessages.push(new Uint8Array(event.data as ArrayBuffer));
+            }
+
+            if (clientMessages.length === 2) {
+                ws.close();
+            } else {
+                ws.send(bytes("binary"));
+            }
+        };
+
+        await until(() => serverMessages.length === 2 && clientMessages.length === 2 && !!closeEvent);
+
+        strictEqual(errorEvent, undefined);
+        strictEqual(closeEvent?.type, "close");
+        strictEqual(closeEvent?.wasClean, true);
+        try {
+            strictEqual(closeEvent?.code, 1000);
+        } catch {
+            strictEqual(closeEvent?.code, 1005);
+        }
+        strictEqual(serverMessages[0], "text");
+        strictEqual(text(serverMessages[1] as Uint8Array), "binary");
+        strictEqual(clientMessages[0], "client sent: text");
+        strictEqual(text(clientMessages[1] as Uint8Array), "client sent: binary");
+    }));
 });
