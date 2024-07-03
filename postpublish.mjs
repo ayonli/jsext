cc75f17c9f92f56b58e0c0a38d5aa761852d2bb7
A-yon Lee <the@ayon.li>
2024-07-03 14:29:06 +0800

    refine package.json

--- postpublish.mjs
+++
@@ -1,6 +0,0 @@
-import * as fs from "fs";
-
-const pkg = JSON.parse(fs.readFileSync("./package.json", "utf8"));
-
-pkg.type = "module";
-fs.writeFileSync("./package.json", JSON.stringify(pkg, null, "    ") + "\n", "utf8");
--- prepublish.mjs
+++
@@ -1,6 +0,0 @@
-import * as fs from "fs";
-
-const pkg = JSON.parse(fs.readFileSync("./package.json", "utf8"));
-
-delete pkg.type;
-fs.writeFileSync("./package.json", JSON.stringify(pkg, null, "    ") + "\n", "utf8");
