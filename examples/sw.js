5cadc212587a4a1d5bed1665e642e34c2479293a
A-yon Lee <i@hyurl.com>
2024-08-09 00:15:28 +0800

    update service worker example for http.serve

--- examples/sw.js
+++ examples/sw.js
@@ -1,11 +1,12 @@
 import { serve } from "../esm/http.js";
+import _try from "../esm/try.js";
 
 serve({
     /**
      * @param {Request} req 
      * @param {import("../types/http").RequestContext} ctx
      */
-    fetch(req, ctx) {
+    async fetch(req, ctx) {
         const { pathname } = new URL(req.url);
 
         if (pathname === "/hello") {
@@ -24,7 +25,30 @@ serve({
 
             return response;
         } else {
-            return fetch(req);
+            const [err, res] = await _try(fetch(req));
+
+            if (err) {
+                const cache = await caches.match(req);
+
+                if (cache) {
+                    return cache;
+                } else {
+                    return new Response("Service Unavailable", { status: 503 });
+                }
+            } else if (!res.ok) {
+                const cache = await caches.match(req);
+
+                if (cache) {
+                    return cache;
+                } else {
+                    return res;
+                }
+            } else {
+                const cache = await caches.open("v1");
+                ctx.waitUntil(cache.put(req, res.clone()));
+
+                return res;
+            }
         }
     }
 });
