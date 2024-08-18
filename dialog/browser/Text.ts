dbad0e403036ccacb8b7b45a04c347c91cdacac1
A-yon Lee <i@hyurl.com>
2024-08-18 20:06:28 +0800

    fix dialog overflow in the browser

--- dialog/browser/Text.ts
+++ dialog/browser/Text.ts
@@ -3,5 +3,7 @@ export default function Text(message: string) {
     text.innerHTML = message.replace(/ /g, "&nbsp;").replace(/\n/g, "<br />");
     text.style.margin = "0 0 1rem";
     text.style.fontSize = "1em";
+    text.style.width = "100%";
+    text.style.overflowX = "auto";
     return text;
 }
--- esm/dialog/browser/Text.js
+++ esm/dialog/browser/Text.js
@@ -3,6 +3,8 @@ function Text(message) {
     text.innerHTML = message.replace(/ /g, "&nbsp;").replace(/\n/g, "<br />");
     text.style.margin = "0 0 1rem";
     text.style.fontSize = "1em";
+    text.style.width = "100%";
+    text.style.overflowX = "auto";
     return text;
 }
 
